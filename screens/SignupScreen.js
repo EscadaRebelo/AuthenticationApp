fcac20ea8b92a11ab0ff2aa5e2c34655e676ebcc
Rodrigo Rebelo <rebelo1919@hotmail.com>
2023-08-02 13:42:12 +0100

    initial commit

---
+++ screens/SignupScreen.js
@@ -0,0 +1,37 @@
+import { useContext, useState } from 'react';
+import { Alert } from 'react-native';
+
+import { createUser } from '../util/auth';
+
+import AuthContent from '../components/Auth/AuthContent';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
+import { AuthContext } from '../store/auth-context';
+
+
+function SignupScreen() {
+    const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+    const authCtx = useContext(AuthContext);
+
+    async function signupHandler({ email, password }) {
+        setIsAuthenticating(true);
+        try {
+            const token = await createUser(email, password);
+            authCtx.authenticate(token);
+        } catch (error) {
+            Alert.alert(
+                'Authentication failed.',
+                'Could not create user, please check your input and try again later!'
+            );
+            setIsAuthenticating(false);
+        }
+    }
+
+    if (isAuthenticating) {
+        return <LoadingOverlay message="Creating user..." />;
+    }
+
+    return <AuthContent onAuthenticate={signupHandler} />;
+}
+
+export default SignupScreen;
